77b40a67ec9fec75a62b943e7e48623dd9004929
SamirN17 <113397409+SamirN17@users.noreply.github.com>
2024-09-15 18:47:10 -0400

    Added proper error handling, cleaned up code

--- PortScanner.java
+++ PortScanner.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 import java.io.IOException;
 import java.net.Socket;
+import java.net.InetAddress;
 
 public class PortScanner {
     public static void main(String[] args) {
@@ -8,31 +9,42 @@ public class PortScanner {
 
         Scanner scanner = new Scanner(System.in);
 
+        // IP address is input, and returned if it is an invalid IP Address
         System.out.println("Enter target IP Address: ");
         String ipAddress = scanner.nextLine();
+        if (!isValidIPAddress(ipAddress)) {
+            System.out.println("Invalid IP address. Please enter a valid IP address.");
+            return;
+        }
 
         System.out.println("Enter start port: ");
-        // if (scanner.hasNextInt()) {
-        //     int port = scanner.nextInt();
-        //     System.out.println("You entered: " + port);
-        // } else {
-        //     System.out.println("Invalid input. Please enter a valid port number.");
-        // }
         int startPort = scanner.nextInt();
         System.err.println("Enter end port: ");
         int endPort = scanner.nextInt();
         
 
-        // System.err.println("Scanning: " + ipAddress + " from port " + startPort + " to port " + endPort);
-
+        System.err.printf("Scanning %s from port %d to port %d...%n\n", ipAddress, startPort, endPort);
         for (int port = startPort; port <= endPort; port++) {
-            try {
-                Socket socket = new Socket(ipAddress, port);
-                System.out.println("Port " + port + " is open.");
-                socket.close();
-            } catch (IOException e) {
-                System.out.println("Port " + port + " is closed.");
-            }
+            scanPort(ipAddress, port);
+        }
+    }
+
+    public static boolean isValidIPAddress(String ipAddress) {
+        try {
+            InetAddress inet = InetAddress.getByName(ipAddress);
+            return inet != null;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public static void scanPort(String ipAddress, int port) {
+        try{
+            Socket socket = new Socket(ipAddress, port);
+            System.out.printf("Port %d is open.%n", port);
+            socket.close();
+        } catch (IOException e) {
+            System.out.printf("Port %d is closed.%n", port);
         }
     }
 }
